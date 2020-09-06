ef335f3ff54db677da4297c387786be7f5af6a48
Vamsi995 <saivamsi.ds123@gmail.com>
2020-09-06 10:33:55 +0530

    Creating matrix math functions

--- src/main.c
+++ src/main.c
@@ -30,40 +30,43 @@ typedef struct matrix
 
 } IMAGE;
 
-
 typedef IMAGE *MATRIX;
 
 
+int sub(MATRIX P, MATRIX A, MATRIX B);
+int dotproduct(MATRIX P, MATRIX A, MATRIX B);
+int addition(MATRIX P, MATRIX A, MATRIX B);
 
-void write(int width, int height, MATRIX image) {
-    FILE *fp = fopen("level.ppm","wb");
+
+
+
+void write(int width, int height, MATRIX image)
+{
+    FILE *fp = fopen("level.ppm", "wb");
 
     int error = 1;
     if (fp != NULL)
     {
         fprintf(fp, "P3\n");
         fprintf(fp, "%d %d\n", width, height);
-        fprintf(fp, "%d\n", 255); 
-        
+        fprintf(fp, "%d\n", 255);
+
         for (int v = 0; v < height; v++)
         {
             for (int h = 0; h < width; h++)
             {
-                fprintf(fp,"%hu %hu %hu ", image->image[v][h].red,image->image[v][h].green, image->image[v][h].blue);
+                fprintf(fp, "%hu %hu %hu ", image->image[v][h].red, image->image[v][h].green, image->image[v][h].blue);
             }
             fprintf(fp, "\n");
         }
 
-        if (fwrite("\n",sizeof(char),1,fp) == 1)
+        if (fwrite("\n", sizeof(char), 1, fp) == 1)
             error = 0;
         fclose(fp);
     }
 
     return;
 }
-
-
-
 
 int main(int argc, char *argv[])
 {
@@ -77,11 +80,11 @@ int main(int argc, char *argv[])
     int maxColor;
     char *fileFormat = NULL;
 
-    if(fgets(space, 4, file) != NULL){
+    if (fgets(space, 4, file) != NULL)
+    {
         fileFormat = space;
         fileFormat[strlen(space) - 1] = '\0';
     }
-
 
     // Raw Version
 
@@ -123,32 +126,162 @@ int main(int argc, char *argv[])
         printf("Error: Not a ppm format file");
     }
 
-
-
     IMAGE *image = (IMAGE *)malloc(sizeof(IMAGE));
 
     image->height = height;
     image->width = width;
-    image->maxColor = maxColor; 
-    image->image = (PIXEL **)malloc(height * sizeof(PIXEL)); 
+    image->maxColor = maxColor;
+    image->image = (PIXEL **)malloc(height * sizeof(PIXEL));
 
-    unsigned short red; unsigned short green; unsigned short blue;
+    unsigned short red;
+    unsigned short green;
+    unsigned short blue;
 
-    for(int i=0;i<height;i++) {
-            image->image[i] = (PIXEL *)malloc(width * sizeof(PIXEL)); 
-        for(int j=0;j<width;j++) // Important bug fix here!
+    for (int i = 0; i < height; i++)
+    {
+        image->image[i] = (PIXEL *)malloc(width * sizeof(PIXEL));
+        for (int j = 0; j < width; j++) // Important bug fix here!
         {
-            fscanf(file,"%hu %hu %hu",&red, &green, &blue);
+            fscanf(file, "%hu %hu %hu", &red, &green, &blue);
             image->image[i][j].red = red;
             image->image[i][j].green = green;
             image->image[i][j].blue = blue;
         }
-    } 
-    
-    fclose(file);
-    
+    }
 
-    write(width,height,image);
-    
+    fclose(file);
+
+    write(width, height, image);
+}
+
+int addition(MATRIX P, MATRIX A, MATRIX B)
+{
+    //Elementwise multiplication
+    if(A->height != B->height || A->width != B->width)
+    {
+        return 1;
+    }
+
+
+    P->height = A->height;
+    P->width = A->width;
+    P->maxColor = A->maxColor;
+
+    unsigned short r,g,b;
+    int m = A->maxColor;
+
+    for (int i = 0; i < A->height; i++)
+    {
+
+        for (int j = 0; j < A->width; j++)
+        {
+
+
+               r = A->image[i][j].red + B->image[i][j].red; 
+             g = A->image[i][j].green + B->image[i][j].green;
+            b = A->image[i][j].blue + B->image[i][j].blue;
+
+                r = (r>m)?m:r;
+                g = (g>m)?m:g;
+                b = (b>m)?m:b;
+
+
+            P->image[i][j].red = r;
+            P->image[i][j].green = g;
+            P->image[i][j].blue = b;
+        }
+    }
+
+    return 0;
 
 }
+
+
+
+int dotproduct(MATRIX P, MATRIX A, MATRIX B)
+{
+     if(A->height != B->height || A->width != B->width)
+    {
+        return 1;
+    }
+
+
+    P->height = A->height;
+    P->width = A->width;
+    P->maxColor = A->maxColor;
+
+    unsigned short r,g,b;
+    int m = A->maxColor;
+
+    for (int i = 0; i < A->height; i++)
+    {
+
+        for (int j = 0; j < A->width; j++)
+        {
+
+
+               r = A->image[i][j].red * B->image[i][j].red; 
+             g = A->image[i][j].green * B->image[i][j].green;
+            b = A->image[i][j].blue * B->image[i][j].blue;
+
+                r = (r>m)?m:r;
+                g = (g>m)?m:g;
+                b = (b>m)?m:b;
+
+
+            P->image[i][j].red = r;
+            P->image[i][j].green = g;
+            P->image[i][j].blue = b;
+        }
+    }
+
+    return 0;
+
+}
+
+
+
+int sub(MATRIX P, MATRIX A, MATRIX B)
+{
+     if(A->height != B->height || A->width != B->width)
+    {
+        return 1;
+    }
+
+
+    P->height = A->height;
+    P->width = A->width;
+    P->maxColor = A->maxColor;
+
+    unsigned short r,g,b;
+    int m = A->maxColor;
+
+    for (int i = 0; i < A->height; i++)
+    {
+
+        for (int j = 0; j < A->width; j++)
+        {
+
+
+               r = A->image[i][j].red - B->image[i][j].red; 
+             g = A->image[i][j].green - B->image[i][j].green;
+            b = A->image[i][j].blue - B->image[i][j].blue;
+
+                r = (r>m)?m:r;
+                g = (g>m)?m:g;
+                b = (b>m)?m:b;
+
+
+            P->image[i][j].red = r;
+            P->image[i][j].green = g;
+            P->image[i][j].blue = b;
+        }
+    }
+
+    return 0;
+
+}
+
+
+
+
